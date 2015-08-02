2a0dc7dcce5f8e5eeda1f793b2e375010b966121
Jeannie <jeannie@kickpunchcreative.com>
2015-08-02 09:54:22 -0500

    got blades to work correctly

--- public/index.js
+++ public/index.js
@@ -2,37 +2,59 @@ mini = document.getElementById("mini_web_image");
 cool = document.getElementById("part2");
 arrow = document.getElementById("arrow");
 blades = document.getElementById("blades");
+info = document.getElementById("infographic");
+light = document.getElementById("lightning");
+circle = document.getElementById("circle");
 /*isInViewport(elem);*/ // Boolean: returns true/false
 
 window.onscroll=inView;
 
 function inView () {
- if (isInViewport(mini)) {
-   mini.style.visibility="visible"
-   mini.setAttribute("class","animated bounceInDown");
-   };
- if (isInViewport(cool)) {
+  if (isInViewport(mini)) {
+    mini.style.visibility="visible"
+    mini.setAttribute("class","animated bounce");
+  };
+  // if (isInViewport(cool)) {
+  //    cool.style.visibility="visible"
+  //   cool.setAttribute("class","animated pulse");
+  // };
+  
+  if (isInViewport(arrow)) {
    
-   cool.setAttribute("class","animated pulse");
-   };
-   if (isInViewport(arrow)) {
+    arrow.setAttribute("class","animated arrow");
+  };
+  if (isInViewport(blades)) {
+
+    blades.setAttribute("class","blades");
+  };
+  
+  if (isInViewport(info)) {
+    info.style.visibility="visible"
    
-   arrow.setAttribute("class","animated arrow");
-   };
-   if (isInViewport(blades)) {
+    info.setAttribute("class","animated zoomInRight");
+  };
+  
+  if (isInViewport(light)) {
    
-   blades.setAttribute("class","blades");
-   };
+    light.setAttribute("class","lightning animated infinite flash");
+  };
+  
+  if (isInViewport(circle)) {
+   
+    circle.setAttribute("class","animated bounce");
+  };
+  
+  
 };
 
 
 
 var isInViewport = function ( elem ) {
-    var distance = elem.getBoundingClientRect();
-    return (
-        distance.top >= 0 &&
-        distance.left >= 0 &&
-        distance.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
-        distance.right <= (window.innerWidth || document.documentElement.clientWidth)
-    );
+  var distance = elem.getBoundingClientRect();
+  return (
+    distance.top >= 0 &&
+    distance.left >= 0 &&
+    distance.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+    distance.right <= (window.innerWidth || document.documentElement.clientWidth)
+  );
 };
